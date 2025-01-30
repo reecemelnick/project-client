5855234ae6bdee310d5133840ce984b1c6e059b2
evin-gg <evin4369@gmail.com>
2025-01-30 12:40:08 -0800

    added a send_packet function

--- include/packet.h
+++ include/packet.h
@@ -2,3 +2,4 @@
 
 void construct_message(struct Message *header, uint8_t type, uint8_t version, uint16_t id, uint16_t length);
 void serialize_message(const struct Message *header, uint8_t username, uint8_t password);
+void send_packet(const int serverfd, const uint8_t * buffer, const size_t size);
