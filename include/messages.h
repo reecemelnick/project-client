c02889636d164f8017462db682da84bd0b33de8c
LouiseYLi <louiseli.van@gmail.com>
2025-02-04 10:14:15 -0800

    added data types and enumerations for client connection protocol

--- include/messages.h
+++ include/messages.h
@@ -7,6 +7,21 @@
 #include <string.h>
 #include <errno.h>
 
+// Client Connection Protocol
+struct ConnectionMessage {
+    // cppcheck-suppress unusedStructMember
+    uint8_t message_type;
+    // cppcheck-suppress unusedStructMember
+    uint8_t version;
+    // cppcheck-suppress unusedStructMember
+    uint8_t server_online;
+    // cppcheck-suppress unusedStructMember
+    uint8_t *active_server_ip;
+};
+// end Client Connection Protocol
+// ====================
+// Message Protocol
+
 struct Message {
     // cppcheck-suppress unusedStructMember
     uint8_t packet_type;
@@ -29,6 +44,13 @@ struct ACC_Create {
     uint8_t *password;
 };
 
+// end Message Protocol
+// ====================
+typedef enum {
+    CLIENT_GETIP = 0,
+    MAN_RETURNIP = 1
+} ConnectionPacketType;
+
 typedef enum {
     BOOLEAN = 1,
     INTEGER = 2,
@@ -37,7 +59,7 @@ typedef enum {
     UTF8STRING = 12,
     SEQUENCE = 48, // Noted in protocol document that they ALWAYS encode it as 48 (30 in hex).
     PRINTABLESTRING = 19,
-    UTCTIME = 23
+    UTCTIME = 23,
 } Tag;
 
 #endif    // MESSAGES_H
