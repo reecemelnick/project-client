4d43de58b9c8f6c17d57bee28f9cc1692fff299f
LouiseYLi <louiseli.van@gmail.com>
2025-02-04 10:46:08 -0800

    added to func name for readability for later protocol implementation

--- src/packet.c
+++ src/packet.c
@@ -21,7 +21,7 @@ void construct_message(struct Message *header, uint8_t type, uint8_t version, ui
     header->payload_len = length;
 }
 
-void serialize_and_send(const int serverfd, const struct Message *header, uint8_t username, uint8_t password)
+void serialize_and_send_message(const int serverfd, const struct Message *header, uint8_t username, uint8_t password)
 {
     size_t packet_size;
 
