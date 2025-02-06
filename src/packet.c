d6750a80fcec36fd0d2b65836b0dd8fd85ee040a
evin-gg <evin4369@gmail.com>
2025-02-06 09:26:46 -0800

    Refactored all files involving acc_create_login to fit the naming scheme to avoid confusion

--- src/packet.c
+++ src/packet.c
@@ -22,7 +22,7 @@ void construct_message(struct Message *header, uint8_t type, uint8_t version, ui
     header->payload_len = length;
 }
 
-void serialize_and_send(const int serverfd, const struct Message *header, uint8_t *username, uint8_t *password)
+void serialize_and_send(int serverfd, const struct ACC_Create_Login *packet)
 {
     size_t packet_size;
 
@@ -33,30 +33,30 @@ void serialize_and_send(const int serverfd, const struct Message *header, uint8_
     uint16_t payload_len_n;
 
     // assign the packet size
-    packet_size = (size_t)HEADER_SIZE + header->payload_len;
+    packet_size = (size_t)HEADER_SIZE + packet->message->payload_len;
 
     // malloc for each corresponding buffer
     buffer         = (uint8_t *)malloc(packet_size);
-    payload_buffer = (uint8_t *)malloc(header->payload_len);
+    payload_buffer = (uint8_t *)malloc(packet->message->payload_len);
 
     // copy payload memory into the payload buffer
-    memcpy(payload_buffer, username, sizeof(*username));
-    memcpy(payload_buffer + sizeof(username), password, sizeof(*password));
+    memcpy(payload_buffer, packet->username, sizeof(*packet->username));
+    memcpy(payload_buffer + sizeof(packet->username), packet->password, sizeof(*packet->password));
 
     // convert the uint16_t attributes to network byte order
-    sender_id_n   = htons(header->sender_id);
-    payload_len_n = htons(header->payload_len);
+    sender_id_n   = htons(packet->message->sender_id);
+    payload_len_n = htons(packet->message->payload_len);
 
     // assign the first 2 bytes of the packet (uint8_t)
-    buffer[0] = header->packet_type;
-    buffer[1] = header->version;
+    buffer[0] = packet->message->packet_type;
+    buffer[1] = packet->message->version;
 
     // assign the next 4 bytes (uint16_t)
     memcpy(buffer + ID_INDEX, &sender_id_n, sizeof(uint16_t));
     memcpy(buffer + LENGTH_INDEX, &payload_len_n, sizeof(uint16_t));
 
     // assign the payload past the header
-    memcpy(buffer + HEADER_SIZE, payload_buffer, sizeof(header->payload_len));
+    memcpy(buffer + HEADER_SIZE, payload_buffer, sizeof(packet->message->payload_len));
 
     // send the buffer
     send_packet(serverfd, buffer, packet_size);
